aad27a93cbe2ccbc28b5434d72cc450a1ca16b2f
awe-fr <rajaouisahel@gmail.com>
2024-08-29 18:31:45 +0200

    parser first

--- sources/main.cpp
+++ sources/main.cpp
@@ -3,32 +3,35 @@
 #include "./../includes/InputParser.hpp"
 
 int main(void) {
-	// WindowsApp *app = new WindowsApp();
+	InputParser input_parser("animations/asf/02.asf", "animations/amc/walk.amc");
+	input_parser.parseAsf();
 
-	// Member *test1 = new Member("test1", 1);
-	// Member *test2 = new Member("test2", 1);
-	// Member *test3 = new Member("test3", 1);
+	WindowsApp *app = new WindowsApp();
+
+	Member *test1 = new Member("test1", 1);
+	Member *test2 = new Member("test2", 1);
+	Member *test3 = new Member("test3", 1);
 	
-	// test2->setPrevious(test1);
-	// test3->setPrevious(test2);
+	test2->setPrevious(test1);
+	test3->setPrevious(test2);
 
-	// glClearColor(0.0f, 0.0f, 0.4f, 0.0f);
+	glClearColor(0.0f, 0.0f, 0.4f, 0.0f);
 
-	// glEnable(GL_DEPTH_TEST);
-	// glDepthFunc(GL_LESS);
-	// glEnable(GL_CULL_FACE);
-	// glCullFace(GL_BACK);
+	glEnable(GL_DEPTH_TEST);
+	glDepthFunc(GL_LESS);
+	glEnable(GL_CULL_FACE);
+	glCullFace(GL_BACK);
 
-	// float ref1 = 180;
-	// float ref2 = 35;
-	// float ref3 = 287;
-	// float ref4 = 90;
+	float ref1 = 180;
+	float ref2 = 35;
+	float ref3 = 287;
+	float ref4 = 90;
 
-	// float mvpPopulated[16];
+	float mvpPopulated[16];
 
-	// MemberList *inst = MemberList::getInstance();
-	// std::vector<Member *> lst = inst->getList();
-	// while(app->isClosed() != true) {
+	MemberList *inst = MemberList::getInstance();
+	std::vector<Member *> lst = inst->getList();
+	while(app->isClosed() != true) {
 
 		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
@@ -39,36 +42,36 @@ int main(void) {
 		app->computeMovement();
 		glUseProgram(app->getProgramID());
 
-	// 	test1->setDegree(ref3, ref1, ref4);
-	// 	test2->setDegree(45, 0, 60);
-	// 	test3->setDegree(45, 60, 0);
+		test1->setDegree(ref3, ref1, ref4);
+		test2->setDegree(45, 0, 60);
+		test3->setDegree(45, 60, 0);
 
-	// 	ref3+= 0.005;
-	// 	ref1+= 0.005;
-	// 	ref4+= 0.005;
+		ref3+= 0.005;
+		ref1+= 0.005;
+		ref4+= 0.005;
 
-	// 	test1->computeTravel();
-	// 	test2->computeTravel();
-	// 	test3->computeTravel();
+		test1->computeTravel();
+		test2->computeTravel();
+		test3->computeTravel();
 
-	// 	for (int i = 0; i < lst.size(); i++) {
-	// 		glBindVertexArray(lst[i]->getVAO());
-	// 		glBindBuffer(GL_ARRAY_BUFFER, lst[i]->getVBO());
-	// 		glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, lst[i]->getIBO());
+		for (int i = 0; i < lst.size(); i++) {
+			glBindVertexArray(lst[i]->getVAO());
+			glBindBuffer(GL_ARRAY_BUFFER, lst[i]->getVBO());
+			glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, lst[i]->getIBO());
 
-	// 		mat4 MVP = matMult(app->getProjection(), matMult(app->getView(), lst[i]->getModel()));
+			mat4 MVP = matMult(app->getProjection(), matMult(app->getView(), lst[i]->getModel()));
 
-	// 		populateMat(mvpPopulated, MVP);
+			populateMat(mvpPopulated, MVP);
 
-	// 		GLuint mvp = glGetUniformLocation(app->getProgramID(), "MVP");
-	// 		glUniformMatrix4fv(mvp, 1, GL_FALSE, mvpPopulated);
+			GLuint mvp = glGetUniformLocation(app->getProgramID(), "MVP");
+			glUniformMatrix4fv(mvp, 1, GL_FALSE, mvpPopulated);
 
-	// 		glEnableVertexAttribArray(0);
-	// 		glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, (void*)0);
+			glEnableVertexAttribArray(0);
+			glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, (void*)0);
 
-	// 		glDrawElements(GL_TRIANGLES, 36, GL_UNSIGNED_INT, 0);
-	// 		glDisableVertexAttribArray(0);
-	// 	}
+			glDrawElements(GL_TRIANGLES, 36, GL_UNSIGNED_INT, 0);
+			glDisableVertexAttribArray(0);
+		}
 
 		ImGui::Begin("HumanGL settings");
 		ImGui::Text("test");
@@ -76,12 +79,9 @@ int main(void) {
 
 		ImGui::Render();
 		ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
-	// }
-	// inst->cleanup();
-	// inst->deleteInstance();
-	// delete app;
-	// return (0);
-
-	InputParser input_parser("animations/asf/02.asf", "animations/amc/walk.amc");
-	input_parser.parseAsf();
+	}
+	inst->cleanup();
+	inst->deleteInstance();
+	delete app;
+	return (0);
 }
